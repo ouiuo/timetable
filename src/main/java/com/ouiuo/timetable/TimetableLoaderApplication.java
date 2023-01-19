eb7562f8ae8c63826312449d1ac227bcd344c0f2
Ouiuo <Ouiuo>
2023-01-20 01:45:12 +0300

    first commit

---
+++ src/main/java/com/ouiuo/timetable/TimetableLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.ouiuo.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouiuo/timetable/TimetableLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouiuo.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
