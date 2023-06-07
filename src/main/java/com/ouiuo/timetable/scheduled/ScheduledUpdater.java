4d3b245968602a3de614f9bee3d095d98799d795
Дегтярев Василий Александрович <vdegtyarev@cbgr.ru>
2023-06-07 09:29:58 +0300

    Add some missing properties in application.properties file

--- src/main/java/com/ouiuo/timetable/scheduled/ScheduledUpdater.java
+++ src/main/java/com/ouiuo/timetable/scheduled/ScheduledUpdater.java
@@ -3,22 +3,19 @@ package com.ouiuo.timetable.scheduled;
 
 import com.ouiuo.timetable.updater.GroupUpdateService;
 import com.ouiuo.timetable.updater.TimetableUpdateService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ScheduledUpdater {
 
     private final TimetableUpdateService timetableUpdateService;
 
     private final GroupUpdateService groupUpdateService;
-
-    public ScheduledUpdater(TimetableUpdateService updateService, GroupUpdateService groupUpdateService) {
-        this.timetableUpdateService = updateService;
-        this.groupUpdateService = groupUpdateService;
-    }
 
     @Scheduled(fixedRateString = "${app.scheduled.timetable.time}")
     public void updateTimetable() {
@@ -27,7 +24,7 @@ public class ScheduledUpdater {
 
 
     @EventListener(ApplicationReadyEvent.class)
-    @Scheduled(cron = "${app.scheduled.groups.time}")
+    @Scheduled(fixedRateString = "${app.scheduled.groups.time}")
     public void updateGroups() {
         groupUpdateService.update();
     }
